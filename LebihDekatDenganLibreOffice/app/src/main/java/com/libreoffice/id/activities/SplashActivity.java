7ecc31304d65f4b1cbc8cab6a1a425b0283b9711
Rhony Abdullah Siagian <abdullahrhony@gmail.com>
2016-11-12 17:46:06 +0700

    add logo

---
+++ LebihDekatDenganLibreOffice/app/src/main/java/com/libreoffice/id/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.libreoffice.id.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.libreoffice.id.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
